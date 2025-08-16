6adc356af2234d74fcc0e8f3482a7bfab920b171
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-16 19:47:05 +0000

    Made randomizer that asks questions

--- remberNow.py
+++ remberNow.py
@@ -1,31 +1,33 @@
-#make a points system CHECK
+#make a points system 
 #FIGURE OUT HOW TO USE RANDOM
 #make the result checker function
 #make a way for questions to correspond to answers
 #make questions CHECK
 #make the questions more efficient
+#make an introduction
 import random
+import sys
 
-#question set
+print("Greetings, this is a quiz I made to practice coding")
+print("I made it for fun, and it is inspired by the first project that they are making in CS 3")
+startingInput = input("Do you wish to start? Input Y or N\n")
 
-one = "What is Luke's last name?"
-two = "Where do babies come from?"
-three = "What is the mitochondria?"
-four = "Where is Utah?"
-five = "What is an anime that Ms.LaRose likes?"
+if startingInput == "Y":
+  print("Ok, the quiz will now begin")
+  totalPoints = 0
+else:
+  sys.exit("Oh, then what are you even doing here???")
+  
 
-#answer set
+questionRando = random.randint (1, 5)
 
-aOne = "Murdock"
-aTwo = "Storks" 
-aThree = "THE POWERHOUSE OF THE CELL"
-afour = "The USA"
-afive = "Assasination Classroom"
+
 
 result = False
+  
+  
 
 def questionResult():
-  totalPoints = 0
   if result == False:
     print("You are wrong")
   else:
@@ -33,5 +35,34 @@ def questionResult():
      print("You are correct! You earned 100 POINTS.")
      print("Your current number of POINTS is", totalPoints)
 
-def questionAnswer():
-  
+
+
+#question set
+
+qOne = "What is Luke's last name?"
+qTwo = "Where do babies come from?"
+qThree = "What is the mitochondria?"
+qFour = "Where is Utah?"
+qFive = "What is an anime that Ms.LaRose likes?"
+
+def rando():
+  if questionRando == 1:
+    print(qOne)
+  elif questionRando == 2:
+    print(qTwo)
+  elif questionRando == 3:
+    print(qThree)
+  elif questionRando == 4:
+    print(qFour)
+  else:
+    print(qFive)
+  userAnswer = input("Input answer: \n")
+  questionRando = random.randint (1, 5)
+#answer set
+aOne = "Murdock"
+aTwo = "Storks" 
+aThree = "THE POWERHOUSE OF THE CELL"
+afour = "The USA"
+afive = "Assasination Classroom"
+
+rando()
